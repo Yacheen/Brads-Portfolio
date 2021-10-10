7fcdd57bb5c88539e9c553ac93b3ce6fac819af3
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-10-10 16:17:54 -0600

    added new flex timeline to GAT

--- src/components/GATTimeline.jsx
+++ src/components/GATTimeline.jsx
@@ -1,26 +1,352 @@
 import React from 'react';
 import voidchampion from '../assets/images/GAT/voidjg.png';
+import electricmarksman from '../assets/images/GAT/upcomingmarksman.png';
+import upcomingnewitems from '../assets/images/GAT/honeyfruit.png';
+import newmarksmanitems from '../assets/images/GAT/wildriftmarksmanitems.png';
+import akshan from '../assets/images/GAT/akshan.png';
+import viego from '../assets/images/GAT/viego.png';
+import rell from '../assets/images/GAT/rell.png';
+import marksmanitemupdate from '../assets/images/GAT/marksmanitemupdate.png';
+import samira from '../assets/images/GAT/samira.png';
+import proplaymatrix from '../assets/images/GAT/proplaymatrix.png';
+import wildriftdesignriskmatrix from '../assets/images/GAT/wildriftdesignriskmatrix.png';
+import wildriftbalance from '../assets/images/GAT/wildriftbalance.png';
+import senna from '../assets/images/GAT/senna.png';
+import allstars from '../assets/images/GAT/allstars.png';
+import omnistone from '../assets/images/GAT/omnistone.png';
+import preseasondragons from '../assets/images/GAT/preseasondragons.png';
 
 export default function GATTimeline() {
     return (
         <section className="flex-timeline">
             <div className="flex-timeline-content">
-                
+                <div className="pillar-line"></div>
+                <div className="flex-timeline-date">
+                    2022
+                </div>
                 <div className="flex-timeline-component-big"> 
-                <di className="flex-timeline-bridge">
-                    <div className="flex-timeline-date">2022</div>
+                        <div className="flex-timeline-description">
+                            <p className="double-line">Upcoming: Overwhelming new jungler that<br/>should fill a void in all your hearts</p>
+                        </div>
+                    
 
-                    <div className="flex-timeline-description">
-                        <p>Upcoming: Overwhelming new jungler that should fill a void in all your hearts</p>
+                    <div className="flex-timeline-image">
+                        <img src={voidchampion} alt="" />
                     </div>
-                </di>
+                </div>
 
-                <div className="flex-timeline-image">
-                    <img src={voidchampion} alt="" />
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p>Upcoming: "Electrifying Marksman"</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={electricmarksman} alt="" />
+                    </div>
                 </div>
+
+                <div className="flex-timeline-date">
+                    2021
                 </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p>Wild Rift: Upcoming new items</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={upcomingnewitems} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Wild Rift: Upcoming new champs</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Upcoming Rengar changes</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p className="double-line">Preseason 2022 Hextech/Chemtech<br/>Dragon Combat Quirks</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={preseasondragons} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p className="double-line">Preseason 2022 Hextech/Chemtech Map State Terrain</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Worlds Jungle Assassins buffs</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Smite Burst Healing nerf</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Wild Rift: Akshan, Brand, Nunu, Thresh</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p>Wild Rift: New Marksmen items</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={newmarksmanitems} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Wild Rift: Xin Zhao and Wukong parity with PC</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p className="double-line">Wild Rift: Phase Rush, Font of Life, and minor rune update</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Wild Rift: Lucian, Senna</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Wild Rift: Fighter Item Update</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Wild Rift: Kha’zix, Rammus, Rengar, Renekton</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p>Akshan</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={akshan} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p>Viego</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={viego} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-date">
+                    2020
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p>Rell</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={rell} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>AP Twitch</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p>Preseason 2021 Marksmen Item Update</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={marksmanitemupdate} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Unreleased Pyke changes</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p>Samira</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={samira} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p className="double-line">Pro Play Matrix (predicts what champions will be played in pro based off data)</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={proplaymatrix} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Ghost extension buffs</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Unreleased Summoner Spells</p>
+                    </div>
+                </div>
+
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Unreleased Sivir changes</p>
+                    </div>
+                </div>
+
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Marksmen class buffs</p>
+                    </div>
+                </div>
+
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Unreleased Nasus changes</p>
+                    </div>
+                </div>
+
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p className="double-line">Funnel investigation and nerf (Monster Hunter debuff changes)</p>
+                    </div>
+                </div>
+
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Soraka top nerfs/changes</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p className="double-line">Wild Rift: Design Risk Matrix (predicts risk of changes to champion design for mobile release)</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={wildriftdesignriskmatrix} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p className="double-line">Wild Rift: General Balance changes (now to end of GAT work)</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={wildriftbalance} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p className="double-line">Senna AD vs Support balance levers (soul drop rate)</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-date">
+                    2019
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p>Senna</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={senna} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p>All-Stars 1v1 Game Mode</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={allstars} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-big"> 
+                        <div className="flex-timeline-description">
+                            <p>Omnistone</p>
+                        </div>
+                    
+                    <div className="flex-timeline-image">
+                        <img src={omnistone} alt="" />
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Rift Herald respawn changes</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Aftershock and Conqueror Changes</p>
+                    </div>
+                </div>
+
+                <div className="flex-timeline-component-small">
+                    <div className="flex-timeline-description">
+                        <p>Akali targeted R1 change</p>
+                    </div>
+                </div>
+
             </div>
-            
         </section>
     )
 }
