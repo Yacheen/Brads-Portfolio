2317e64deae44c512a50a5e7c940a48841458e0e
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-10-11 01:06:16 -0600

    edited thankyou message

--- src/components/Thankyou.jsx
+++ src/components/Thankyou.jsx
@@ -5,7 +5,7 @@ export default function Thankyou() {
         <section className="thankyou">
             <div className="thankyou-content">
                 <h2>Thank you!</h2>
-                <h3>Your Email has bees been sent! <br /> I will get back to you when I can </h3>
+                <h3>Your Email has been sent.</h3>
             </div>
             
         </section>
