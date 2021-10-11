38e5a4d24f89f3ca31298f8309a3fd792848a27a
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-10-10 22:15:04 -0600

    Added readme and edited footer

---
+++ README.md
@@ -0,0 +1,3 @@
+Welcome to Brad's portfolio!
+
+You can see it here at https://blizz.lol
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,25 @@ export default function Footer() {
     return (
         <footer className="footer-container">
             <div className="footer-content">
-                <div className="footer-link">
-                    <i class="fab fa-twitch"></i>
-                    <p>Twitch</p>
-                </div>
-                <div className="footer-link">
-                    <i class="fab fa-twitter"></i>
-                    <p>Twitter</p>
-                </div>
-                
-                <div className="footer-link">
-                    <i class="fab fa-linkedin"></i>
-                    <p>LinkedIn</p>
-                </div>
+                <a href="https://www.twitch.tv/blizz_lol" target="_blank" rel="noreferrer">
+                    <div className="footer-link">
+                        <i class="fab fa-twitch"></i>
+                        <p>Twitch</p>
+                    </div>
+                </a>
+                <a href="https://twitter.com/blizz_lol" target="_blank" rel="noreferrer">
+                    <div className="footer-link">
+                        <i class="fab fa-twitter"></i>
+                        <p>Twitter</p>
+                    </div>
+                </a>
+
+                <a href="https://www.linkedin.com/in/brad-dallaire-881a58155?trk=public_profile_browsemap" target="_blank" rel="noreferrer">
+                    <div className="footer-link">
+                        <i class="fab fa-linkedin"></i>
+                        <p>LinkedIn</p>
+                    </div>
+                </a>
             </div>
         </footer>
     )
