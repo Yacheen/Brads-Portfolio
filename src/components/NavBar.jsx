cae888507ff15bee7de67374ad1d43b87951b18d
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-10-16 06:21:35 -0600

    added mobile media queries to site

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,11 +19,11 @@ export default function NavBar() {
                     <Link to="/"><div className="nav-bar-name" onClick={() => setCurrentTab(0)}>Brad "Blizz" Dallaire</div></Link>
                         
                     <ul className="nav-bar-links">
-                        <Link to="/"><li  className={currentTab === 0 ? "toggled-button" : "none"} onClick={() => setCurrentTab(0)}>Home</li></Link>
-                        <Link to="/TFT"><li className={currentTab === 1 ? "toggled-button" : "none"} onClick={() => setCurrentTab(1)}>TFT</li></Link>
-                        <Link to="/GAT"><li className={currentTab === 2 ? "toggled-button" : "none"} onClick={() => setCurrentTab(2)}>GAT</li></Link>
-                        <Link to="/Esports"><li className={currentTab === 3 ? "toggled-button" : "none"} onClick={() => setCurrentTab(3)}>Esports</li></Link>
-                        <Link to="/Contact"><li className={currentTab === 4 ? "toggled-button" : "none"} onClick={() => setCurrentTab(4)}>Contact</li></Link>
+                        <div className="home-button"><Link to="/"><li className={currentTab === 0 ? "toggled-button nav-link" : "none nav-link"} onClick={() => setCurrentTab(0)}>Home</li></Link></div>
+                        <Link to="/TFT"><li className={currentTab === 1 ? "toggled-button nav-link" : "none nav-link"} onClick={() => setCurrentTab(1)}>TFT</li></Link>
+                        <Link to="/GAT"><li className={currentTab === 2 ? "toggled-button nav-link" : "none nav-link"} onClick={() => setCurrentTab(2)}>GAT</li></Link>
+                        <Link to="/Esports"><li className={currentTab === 3 ? "toggled-button nav-link" : "none nav-link"} onClick={() => setCurrentTab(3)}>Esports</li></Link>
+                        <Link to="/Contact"><li className={currentTab === 4 ? "toggled-button nav-link" : "none nav-link"} onClick={() => setCurrentTab(4)}>Contact</li></Link>
                     </ul>
                 </div>
             </section>
