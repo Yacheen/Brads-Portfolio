6758ede700f714b81c017e044207da86bbb81725
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-11-12 21:15:05 -0700

    Added new buttons to all viewports, thanks to @CloudSmithy_

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,12 +6,21 @@ import GATTimeline from './GATTimeline';
 import EsportsTimeline from './EsportsTimeline';
 import Contact from './Contact';
 import Thankyou from './Thankyou';
-
+//buttons
+import akshanhome from '../assets/images/navbarButtons/akshanhome.png';
+import akshanhome2 from '../assets/images/navbarButtons/akshanhome2.png';
+import RellGAT from '../assets/images/navbarButtons/RellGAT.png';
+import RellGAT2 from '../assets/images/navbarButtons/RellGAT2.png';
+import SamiraEsports from '../assets/images/navbarButtons/SamiraEsports.png';
+import SamiraEsports2 from '../assets/images/navbarButtons/SamiraEsports2.png';
+import SennaContact from '../assets/images/navbarButtons/SennaContact.png';
+import SennaContact2 from '../assets/images/navbarButtons/SennaContact2.png';
+import ViegoTFT from '../assets/images/navbarButtons/ViegoTFT.png';
+import ViegoTFT2 from '../assets/images/navbarButtons/ViegoTFT2.png';
 
 export default function NavBar() {
     const [currentTab, setCurrentTab] = useState(0);
-    
-
+    //on css,
     return (
         <Router>
             <section className="nav-bar" data-aos="zoom-in" data-aos-duration="1000">
@@ -19,11 +28,11 @@ export default function NavBar() {
                     <Link to="/"><div className="nav-bar-name" onClick={() => setCurrentTab(0)}>Brad "Blizz" Dallaire</div></Link>
                         
                     <ul className="nav-bar-links">
-                        <div className="home-button"><Link to="/"><li className={currentTab === 0 ? "toggled-button nav-link" : "none nav-link"} onClick={() => setCurrentTab(0)}>Home</li></Link></div>
-                        <Link to="/TFT"><li className={currentTab === 1 ? "toggled-button nav-link" : "none nav-link"} onClick={() => setCurrentTab(1)}>TFT</li></Link>
-                        <Link to="/GAT"><li className={currentTab === 2 ? "toggled-button nav-link" : "none nav-link"} onClick={() => setCurrentTab(2)}>GAT</li></Link>
-                        <Link to="/Esports"><li className={currentTab === 3 ? "toggled-button nav-link" : "none nav-link"} onClick={() => setCurrentTab(3)}>Esports</li></Link>
-                        <Link to="/Contact"><li className={currentTab === 4 ? "toggled-button nav-link" : "none nav-link"} onClick={() => setCurrentTab(4)}>Contact</li></Link>
+                        <div className="home-button"><Link to="/"><li className={`button-settings nav-link home-link ${currentTab === 0 ? "home-toggled" : "none"} `} onClick={() => setCurrentTab(0)}></li></Link></div>
+                        <Link to="/TFT"><li className={`button-settings nav-link tft-link ${currentTab === 1 ? "tft-toggled" : "none"}`} onClick={() => setCurrentTab(1)}></li></Link>
+                        <Link to="/GAT"><li className={`button-settings nav-link gat-link ${currentTab === 2 ? "gat-toggled" : "none"}`} onClick={() => setCurrentTab(2)}></li></Link>
+                        <Link to="/Esports"><li className={`button-settings nav-link esports-link ${currentTab === 3 ? "esports-toggled" : "none"}`} onClick={() => setCurrentTab(3)}></li></Link>
+                        <Link to="/Contact"><li className={`button-settings nav-link contact-link ${currentTab === 4 ? "contact-toggled" : "none"}`} onClick={() => setCurrentTab(4)}></li></Link>
                     </ul>
                 </div>
             </section>
